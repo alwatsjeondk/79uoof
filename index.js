2b3f02687b4bf14d4d0cd3d56f769e31869bc174
alwatsjeondk <always.be.happy.3939@gmail.com>
2025-04-02 22:50:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -36,3 +36,11 @@ app.get("/form", (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
+/*
+fetch("https://api64.ipify.org?format=json")
+  .then(response => response.json())
+  .then(data => console.log("Your IP:", data.ip))
+  .catch(error => console.error("Error fetching IP:", error));
+
+  */
