cda455ed57fa1c655c252a219b33ed42c7abcafe
alwatsjeondk <always.be.happy.3939@gmail.com>
2025-04-02 22:44:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,28 +9,29 @@ const PORT = process.env.PORT || 3000;
 app.use(requestIp.mw());
 
 app.get("/form", (req, res) => {
-    var name = req.query.name;
-    const clientIp = req.clientIp || req.headers["x-forwarded-for"] || req.connection.remoteAddress;
-    
-    // Get local IPs
-    const networkInterfaces = os.networkInterfaces();
-    let localIps = [];
-    
-    Object.keys(networkInterfaces).forEach((iface) => {
-        networkInterfaces[iface].forEach((ifaceDetails) => {
-            if (!ifaceDetails.internal) {
-                localIps.push(ifaceDetails.address);
-            }
-        });
-    });
+   try {
+        const clientIp = req.clientIp || req.headers["x-forwarded-for"] || req.connection.remoteAddress;
 
-    console.log(name);
-    console.log(`publicIp: ${clientIp}`);
-    console.log(`localIps: ${localIps}`);
-    console.log("--------------");
-    
-    res.send(`<h1>${name} TERA LOCATION NIKAL LUNGA 😙</h1>`);
-});
+        // Use a public IP if running locally (since localhost IPs won't work)
+        const ipToCheck = clientIp === "::1" ? "8.8.8.8" : clientIp;
+
+        // Fetch location data
+        const response = await axios.get(`http://ip-api.com/json/${ipToCheck}`);
+        const locationData = response.data;
+
+        res.json({
+            ip: ipToCheck,
+            country: locationData.country,
+            region: locationData.regionName, // State/Province
+            city: locationData.city,
+            latitude: locationData.lat,
+            longitude: locationData.lon,
+            isp: locationData.isp
+        });
+    } catch (error) {
+        res.status(500).json({ error: "Could not retrieve location data" });
+    } 
+   });
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
